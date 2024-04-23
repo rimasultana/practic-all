310a5664c7e9141d3365ce5f74c55130b91bc29c
rimasultana <rimasultana20225@gmail.com>
2024-04-23 19:55:19 +0600

    new added

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-var numbers = [45, 45, 66, 31, 90];
-numbers.push(50);
+// var numbers = [45, 45, 66, 31, 90];
+// numbers.push(50);
 
-console.log(numbers);
+// console.log(numbers);
 
-var friends = ['Rima', 'Nodhi', 'Joita'];
+// var friends = ['Rima', 'Nodhi', 'Joita'];
 
-friends.push('Mobasshir');
-friends.push('Rahim');
-friends.push('Arif');
-// 
- friends.shift();
- friends.shift();
+// friends.push('Mobasshir');
+// friends.push('Rahim');
+// friends.push('Arif');
+// // 
+//  friends.shift();
+//  friends.shift();
 
- friends.unshift('Abir');
- friends.unshift('Ovi');
- console.log(friends);
+//  friends.unshift('Abir');
+//  friends.unshift('Ovi');
+//  console.log(friends);
+
+
+
+ console.log(5 < 6);
+ console.log(5 > 6);
+
+ console.log(5 <= 6);
+ console.log(5 >= 6);
+
+ console.log(Rima);
