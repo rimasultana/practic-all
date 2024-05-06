b9def886d495e14d2dd27c25b97479f51f72a294
rimasultana <rimasultana20225@gmail.com>
2024-05-06 16:53:33 +0800

    loop

--- index.js
+++ index.js
@@ -1,42 +1,6 @@
-var myMoney = 19000;
-
-var phonePrize = 2500;
-
-if(myMoney < phonePrize){
-    console.log("ami Iphone kinbo")
-}else{
-    console.log("normal Phone kinbo")
-};
-
-
-var Graduation = false;
-
-var salary = 2500;
-
-var cars = 1;
-
-if(Graduation === true || salary > 5000 || cars >= 1){
-    console.log("asho biya kori")
-}else{
-    console.log("tor kopale biya nai!!!!")
+var rostGiven = 0;
+while(rostGiven < 8 ) {
+    console.log('Rost den, please')
+    console.log(rostGiven)
+    rostGiven++;
 }
-
-var money = 15;
-var singara = 35;
-var pototo = 20;
-var chomocha = 15;
-
-if(singara < money){
-    console.log("singara khabo")
-}
-else if(pototo < money){
-    console.log("potato khabo")
-}
-else if(chomocha){
-    console.log("chomucha khabo")
-}
-else{
-    console.log('cha khabo')
-}
-
-
