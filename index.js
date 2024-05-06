ba357164a09a61183e5d718825301981e2caad3d
rimasultana <rimasultana20225@gmail.com>
2024-05-06 17:13:57 +0800

    while loop

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ console.log("hello")
 console.log(num);
 num++;
 }
+
+
+var rostGiven = 0;
+while(rostGiven < 7 ){
+    console.log("Rost den, please")
+    console.log(rostGiven)
+    rostGiven++;
+
+}
