1ea4981e2ba80e24ab93a339b4286ba69ad10f10
rimasultana <rimasultana20225@gmail.com>
2024-05-17 19:42:35 +0600

    for loop

--- whileLoop.js
+++ whileLoop.js
@@ -1,8 +1,19 @@
-var rostGiven = 0;
+// var rostGiven = 0;
 
-while(rostGiven < 7) {
-    console.log("Rost den, please")
-    console.log(rostGiven)
-    rostGiven++;
+// while(rostGiven < 7) {
+//     console.log("Rost den, please")
+//     console.log(rostGiven)
+//     rostGiven++;
+// }
+
+// for(rostGiven = 0; rostGiven < 7; rostGiven++){
+//     console.log("Rost den, please!!1")
+// }
+
+
+// simple verson for loop
+console.log("simple verson for for loop")
+for (i = 0; i < 7 ; i++){
+    console.log(i);
 }
 
