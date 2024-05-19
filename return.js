78254a1e04c9d6e6d5730fac7dbff83407c95848
rimasultana <rimasultana20225@gmail.com>
2024-05-19 19:54:08 +0600

    return value

--- return.js
+++ return.js
@@ -1,8 +1,23 @@
-function add (number1, number2){
-    console.log(number1, number2);
-    var sum = number1 + number2;
-    return sum;
+// function add (number1, number2){
+//     console.log(number1, number2);
+//     var sum = number1 + number2;
+//     return sum;
+// }
+
+// var total = add(80, 20);
+// console.log('totol',totol);
+
+
+function getAverage(marks1, marks2, marks3){
+    const total = marks1 + marks2 + marks3;
+    const average= total / 3;
+    return average;
 }
 
-var totol = add(80, 20);
-console.log('totol',totol);
+const marks1 = 60;
+const marks2 = 58;
+const marks3 = 59;
+
+
+var myAverage = getAverage(marks1, marks2, marks3);
+console.log('my average so far:', myAverage);
