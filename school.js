9cf4a416752aa2b78f88e57e158b9263234ee56e
rimasultana <rimasultana20225@gmail.com>
2024-05-24 23:59:52 +0600

    divition

--- school.js
+++ school.js
@@ -1,12 +1,4 @@
-let x = 100 + 50;
-console.log(x);
-
-let a = 100;
-let b = 50;
-let y = a + b ;
-console.log(y);
- 
-
-let z = 3;
-let w = (100 + 50) * z;
-console.log(w);
+let  x = 5;
+let y = 2;
+ let z = x / y;
+ console.log(z);
