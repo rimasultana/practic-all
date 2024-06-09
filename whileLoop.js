37c1a29fce799cfe6eb606dcfb54baeafd898166
rimasultana <rimasultana20225@gmail.com>
2024-06-09 21:31:14 +0600

    just amni

--- whileLoop.js
+++ whileLoop.js
@@ -1,39 +1,39 @@
-var rostGiven = 0;
+// var rostGiven = 0;
 
-while(rostGiven < 7) {
-    console.log("Rost den, please")
-    console.log(rostGiven)
-    rostGiven++;
-}
+// while(rostGiven < 7) {
+//     console.log("Rost den, please")
+//     console.log(rostGiven)
+//     rostGiven++;
+// }
 
-for(rostGiven = 0; rostGiven < 7; rostGiven++){
-    console.log("Rost den, please!!1")
-}
+// for(rostGiven = 0; rostGiven < 7; rostGiven++){
+//     console.log("Rost den, please!!1")
+// }
 
 
-simple verson for loop
-console.log("simple verson for for loop")
-for (i = 0; i < 7 ; i++){
-    console.log(i);
-}
+// simple verson for loop
+// console.log("simple verson for for loop")
+// for (i = 0; i < 7 ; i++){
+//     console.log(i);
+// }
 
-var numbers = [45, 37, 23, 56, 80, 56, 23];
-for(i = 0; i < numbers.length; i++ ){
-    var number = numbers[i];
-    console.log(number);
-    if(number > 50){
-        break;
-    }
-}
+// var numbers = [45, 37, 23, 56, 80, 56, 23];
+// for(i = 0; i < numbers.length; i++ ){
+//     var number = numbers[i];
+//     console.log(number);
+//     if(number > 50){
+//         break;
+//     }
+// }
 
-var items = ['pen', 'book', 'pencile', 'rumal','churi','kfiei','oduf','douiuf','jjdii','idujhnfu']
-for (i = 0; i < items.length; i++){
-    var item = items[i];
-    console.log(item)
-    if(item == 'churi'){
-        break;
-    }
-}
+// var items = ['pen', 'book', 'pencile', 'rumal','churi','kfiei','oduf','douiuf','jjdii','idujhnfu']
+// for (i = 0; i < items.length; i++){
+//     var item = items[i];
+//     console.log(item)
+//     if(item == 'churi'){
+//         break;
+//     }
+// }
 
 var numbers = [10, 38, 48, 89, 84, 84,384,373, 98,13, 34 ,44]
 
