8242f8fc5a77945a8c288cfd23c4c81eb4f9229f
rimasultana <rimasultana20225@gmail.com>
2024-06-25 00:59:22 +0600

    object

--- object.js
+++ object.js
@@ -1,15 +1,24 @@
-const car = {
-    type : "Flat",
-    model : "500",
-    color : "white"
-};
-console.log(car);
+// const car = {
+//     type : "Flat",
+//     model : "500",
+//     color : "white"
+// };
+// console.log(car);
 
-const person = {
-    firstName : "Rima",
-    lastName : "Sultana",
-    age : 20,
-    eyeColor : "blue"
+// const person = {
+//     firstName : "Rima",
+//     lastName : "Sultana",
+//     age : 20,
+//     eyeColor : "blue"
 
-};
-console.log(person);
+// };
+// console.log(person);
+
+
+const person = {};
+person.firstName = "Rima";
+person.lastName = "Sultana";
+person.aga = 20;
+person.eyeColor = "blue";
+
+console.log(person)
