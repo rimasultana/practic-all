d39d528e4c5322ec75a62b41ca2fb5b6cbe21da7
rimasultana <rimasultana20225@gmail.com>
2024-06-24 21:52:58 +0600

    function

--- function.js
+++ function.js
@@ -1,6 +1,19 @@
-function toCelsius(f) {
-    return (5/9) * (f-32);
-  }
-  
-  let value = toCelsius(77);
-  console.log(value);
+// function toCelcease(f) {
+//     return (5 / 9)  * ( f - 32);
+// }
+//  const x = toCelcease(77);
+//  console.log(x);
+
+// function myFunction (a, b){
+// const sum = a + b;
+// console.log(sum);
+// return sum;
+// }
+ 
+// const x = myFunction(2,5);
+// console.log(x);
+
+const x = function (a, b) {
+    return a * b
+}
+console.log(x(2,4));
