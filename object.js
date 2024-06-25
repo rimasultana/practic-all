4439bf0eb4a9ed35854c2fc3cbe73ee37ea96bd9
rimasultana <rimasultana20225@gmail.com>
2024-06-26 01:24:32 +0600

    amnitai

--- object.js
+++ object.js
@@ -10,24 +10,24 @@
 
 //  console.log(myObject.carName.car2);
 
-// const person = {
-//     firstName : "Rima",
-//     lastName : "Sultana",
-//     id : 770688,
-//     fullName : function(){
-//         return this.firstName + " " + this.lastName;
-//     }
-// }
-// console.log(person.fullName());
-
 const person = {
     firstName : "Rima",
     lastName : "Sultana",
-    id : 770688
-};
-//  added a method
- person.name = function(){
-    return (this.firstName + " " + this.lastName).toUpperCase()
- };
+    id : 770688,
+    fullName : function(){
+        return this.firstName + " " + this.lastName;
+    }
+}
+console.log(person.fullName());
+
+// const person = {
+//     firstName : "Rima",
+//     lastName : "Sultana",
+//     id : 770688
+// };
+// //  added a method
+//  person.name = function(){
+//     return (this.firstName + " " + this.lastName).toUpperCase()
+//  };
  
- console.log(" My name is " + person.name());
+//  console.log(" My name is " + person.name());
