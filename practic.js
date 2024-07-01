52bdcc41e353a22179d829a6388a7896e764e64b
rimasultana <rimasultana20225@gmail.com>
2024-07-01 06:35:47 +0600

    object

--- practic.js
+++ practic.js
@@ -1,9 +1,44 @@
-function myFunction(a,b){
-    const sum = a + b;
-    console.log("🚀 ~ myFunction ~ sum:", sum)
-    return sum;
+// const car = {
+//     type: "Fiat",
+//     model:"500",
+//     color:"white"
+// }
+// console.log("This car name is " + car.type);
+
+// const person = {
+//     firstName: "Rima",
+//     lastName: "Sultana",
+//     age: 20,
+//     eyeColor:"blue"
+// };
+// console.log(person.firstName + " is " + person.lastName) ;
+
+
+// empty object
+
+// const person = {};
+
+// person.firstName = "Rima";
+// person.lastName  = "Sultana";
+// person.age = 20;
+// person.eyeColor = "blue";
+
+// console.log(person.firstName + " is " + person.age  + " " + "years old");
+
+
+// const person = new Object();
+// person.firstName = " Roma";
+// person.lastName = "Sultna";
+// person.age = 20;
+// person.eyeColor = "blue";
+//  console.log(person.firstName + " is " + person.age + ' ' + "years old") ; 
+
+const person = {
+    firstName: "Rima",
+    lastName : "Sultana",
+    age : 20,
+
 }
+// console.log("The last Name is " + person.lastName);
 
-console.log("🚀 ~ myFunction:", myFunction(10,5));
-
-
+console.log("The first Name is " + person["firstName"]);
