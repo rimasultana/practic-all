595990e439d3d3c2d3eae4930e958a5594300399
rimasultana <rimasultana20225@gmail.com>
2024-07-01 07:05:33 +0600

    object

--- practic.js
+++ practic.js
@@ -33,12 +33,54 @@
 // person.eyeColor = "blue";
 //  console.log(person.firstName + " is " + person.age + ' ' + "years old") ; 
 
+// const person = {
+//     firstName: "Rima",
+//     lastName : "Sultana",
+//     age : 20,
+
+// }
+// // console.log("The last Name is " + person.lastName);
+
+// console.log("The first Name is " + person["firstName"]);
+
+
+// const person = {
+//     firstName: "Rima",
+//     lastName: "Sultana",
+//     age:20,
+//     fullName: function(){
+//         return this.firstName + " " + this.lastName;
+//     }
+
+// }
+// console.log(person.fullName());
+
+
+// const person = {
+//     firstName: "Rima",
+//     lastName: "Sultana",
+//     age:20,
+// }
+// const x = person;
+
+// x.age = 40;
+//  console.log(person.firstName + person.age);
+
+
+// accessing the object property
+
+// let age = person.age;
+// let age = person["age"];
+// let age = person[x];
+
 const person = {
     firstName: "Rima",
     lastName : "Sultana",
     age : 20,
+    eyeColor: "blue"
+};
 
-}
-// console.log("The last Name is " + person.lastName);
-
-console.log("The first Name is " + person["firstName"]);
+let x = "firstName";
+let y = "age";
+// console.log(person["firstName"]);
+console.log(person[x] + person[y]);
